95fe84b45c498290a2af4dbfc09fe499f79dca2f
stephensalim <ssalim@hutanrimba.net>
2020-11-11 17:34:20 +1100

    App Built

--- Pattern1/README.md
+++ Pattern1/README.md
@@ -7,22 +7,19 @@ aws cloudformation create-stack --stack-name pattern1-base \
                                 --region ap-southeast-2
 ```
 
+```
+aws cloudformation update-stack --stack-name pattern1-base \
+                                --template-body file://pattern1-base.yml \
+                                --region ap-southeast-2
+```
+
+
 1. Deploy Application
 
 ```
 aws cloudformation create-stack --stack-name pattern1-app \
-                                --template-body file://pattern1-application.yml \
+                                --template-body file://pattern1-app.yml \
                                 --parameters ParameterKey=BaselineVpcStack,ParameterValue=pattern1-base \
-                                             ParameterKey=DBMasterUserPassword,ParameterValue=< yourpassword > \
-                                --capabilities CAPABILITY_NAMED_IAM \
-                                --region ap-southeast-2
-```
-
-```
-aws cloudformation create-stack --stack-name pattern1-app \
-                                --template-body file://pattern1-application.yml \
-                                --parameters ParameterKey=BaselineVpcStack,ParameterValue=pattern1-base \
-                                             ParameterKey=DBMasterUserPassword,ParameterValue=P455w0rd#$% \
                                 --capabilities CAPABILITY_NAMED_IAM \
                                 --region ap-southeast-2
 ```
@@ -30,5 +27,16 @@ aws cloudformation create-stack --stack-name pattern1-app \
 
 ```
 curl --header "Content-Type: application/json" --request POST --data '{"Name":"John","Text":"I love dumplings"}' http://localhost:8080/encrypt
-curl --header "Content-Type: application/json" --request GET --data '{"Name":"John","Key":"3ccfef82-f8d7-49f7-acca-8784c2bbd78c"}' http://localhost:8080/decrypt
+curl --header "Content-Type: application/json" --request GET --data '{"Name":"John","Key":"a94cc58c-56c5-464b-8f05-604257bb2336"}' http://localhost:8080/decrypt
+
+curl --header "Content-Type: application/json" --request POST --data '{"Name":"Trump","Text":"I like to eat shit"}' patte-Patte-SPG83OC5YDC-105497302.ap-southeast-2.elb.amazonaws.com/encrypt
+curl --header "Content-Type: application/json" --request GET --data '{"Name":"Trump","Key":"0e673ef2-e993-4626-89f0-02626fb2b8df"}' patte-Patte-SPG83OC5YDC-105497302.ap-southeast-2.elb.amazonaws.com/decrypt
+
+```
+
+```
+$(aws ecr get-login --no-include-email)
+TAG="${SERVICE_NAME}"
+docker build --tag "${REPOSITORY_URI}:${TAG}" .
+docker push "${REPOSITORY_URI}:${TAG}"
 ```
--- Pattern1/app/app.js
+++ Pattern1/app/app.js
@@ -1,6 +1,7 @@
 'use strict';
 const AWS = require('aws-sdk');
 const kmsClient = new AWS.KMS({region:'ap-southeast-2'});
+const secretsmanager = new AWS.SecretsManager({region:'ap-southeast-2'});
 const express = require('express');
 const router = express.Router();
 const bodyParser = require("body-parser");
@@ -8,7 +9,7 @@ var mysql = require('mysql');
 const zlib = require('zlib');
 
 // Constants
-const PORT = 8080;
+const PORT = 80;
 const HOST = '0.0.0.0';
 
 // App
@@ -17,10 +18,32 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const DBHOST = "ppt3yx3vapaj01.cvgtcrewqixd.ap-southeast-2.rds.amazonaws.com"
-const DBUSER = "masteradmin"
-const DBPASS = "P455w0rd#$%"
-const KeyId  = '3ccfef82-f8d7-49f7-acca-8784c2bbd78c';
+const DBHOST = process.env.DBHOST;
+const KeyId  = process.env.KeyId;
+const DBSecret = process.env.DBSecret;
+
+function hydrateDBCreds( DBSecret ){
+  var promise = new Promise(function(resolve,reject){
+    var params = {
+      SecretId: DBSecret
+    };
+    secretsmanager.getSecretValue(params, function(err, data) {
+      if (err){
+        console.log(err);
+        reject(err);
+      } 
+      else{
+        var secString = data['SecretString']
+        var secObj = JSON.parse(secString)
+        process.env.DBUSER = secObj['username']
+        process.env.DBPASS = secObj['password']
+        resolve ( data );
+      }
+                 // successful response
+    });  
+  });
+  return promise;
+};
 
 function encryptData( KeyId, Plaintext ){
   var promise = new Promise(function(resolve,reject){
@@ -38,6 +61,7 @@ function encryptData( KeyId, Plaintext ){
   });
   return promise;
 };
+
 
 function decryptData( KeyId, CiphertextBlob ){
   var promise = new Promise(function(resolve,reject){
@@ -58,31 +82,57 @@ function decryptData( KeyId, CiphertextBlob ){
 
 
 
-function createDB(){
-  var con = mysql.createConnection({host: DBHOST,user: DBUSER,password: DBPASS});
+function decryptData( KeyId, CiphertextBlob ){
   var promise = new Promise(function(resolve,reject){
-    var sql = "CREATE DATABASE IF NOT EXISTS mydb";
-    con.query(sql, function (err, result) {
+    kmsClient.decrypt({ CiphertextBlob, KeyId }, (err, data) => {
       if (err) {
-        reject(err);
+        console.log(err)
+        reject(err); // an error occurred
       }
-      else{
-        resolve("database create done");
-      }
+      else {
+        const { Plaintext } = data;
+        resolve ( Plaintext.toString() );
+      };
     });
+  
   });
   return promise;
 };
 
+
+
+function createDB(DBSecret){
+  
+  var promise = new Promise(function(resolve,reject){
+    var con = mysql.createConnection({host: DBHOST,user: process.env.DBUSER ,password: process.env.DBPASS});
+    var sql = "CREATE DATABASE IF NOT EXISTS mydb";
+    con.query(sql, function (err, result) {
+      if (err) {
+        con.end();
+        reject(err);
+      }
+      else{
+        con.end();
+        resolve("database create done");
+      }
+    });
+  });
+
+
+  return promise;
+};
+
 function createTable(){
-  var con = mysql.createConnection({host: DBHOST,user: DBUSER,password: DBPASS,database: "mydb"});
+  var con = mysql.createConnection({host: DBHOST,user: process.env.DBUSER ,password: process.env.DBPASS,database: "mydb"});
   var promise = new Promise(function(resolve,reject){
     var sql = "CREATE TABLE IF NOT EXISTS peoplesecret (name VARCHAR(244) NOT NULL, secret TEXT, PRIMARY KEY (name) )";
     con.query(sql, function (err, result) {
       if (err) {
+        con.end();
         reject(err);
       }
       else{
+        con.end();
         resolve("table create done");
       }
     });
@@ -91,14 +141,16 @@ function createTable(){
 };
 
 function storeSecret(Payload){
-  var con = mysql.createConnection({host: DBHOST,user: DBUSER,password: DBPASS,database: "mydb"});
+  var con = mysql.createConnection({host: DBHOST,user: process.env.DBUSER ,password: process.env.DBPASS,database: "mydb"});
   var promise = new Promise(function(resolve,reject){
     var sql = "INSERT INTO peoplesecret (name, secret) VALUES ('" + Payload['Name'] + "', '" + Payload['Text'] + "' ) ON DUPLICATE KEY UPDATE name= '"+ Payload['Name']  +"', secret='" +  Payload['Text'] + "'" ;
     con.query(sql, function (err, result) {
       if (err) {
+        con.end();
         reject(err);
       }
       else{
+        con.end();
         resolve("1 record inserted");
       }
     });
@@ -107,18 +159,21 @@ function storeSecret(Payload){
 };
 
 function getSecret(Payload){
-  var con = mysql.createConnection({host: DBHOST,user: DBUSER,password: DBPASS,database: "mydb"});
+  var con = mysql.createConnection({host: DBHOST,user: process.env.DBUSER ,password: process.env.DBPASS,database: "mydb"});
   var promise = new Promise(function(resolve,reject){
     var sql = "SELECT secret from peoplesecret WHERE name='"+ Payload['Name'] +"'";
     con.query(sql, function (err, result) {
       if (err) {
+        con.end();
         reject(err);
       }
       else{
         if(result.length < 1){
-          reject(err);
+          con.end();
+          reject('no record found');
         } 
         else{
+          con.end();
           resolve(result[0].secret);
         }
       }
@@ -128,60 +183,72 @@ function getSecret(Payload){
 };
 
 
+
+router.get('/', (req, res) => {
+      res.status(200).send( 'OK' );
+});
+
+
 router.post('/encrypt', (req, res) => {
   const Payload = {
     'Name': req.body.Name,
     'Text': req.body.Text
   }
-  //var Key = KeyARNPrefix + KeyId
 
-  encryptData(KeyId, Payload['Text'])
-  .then(function(response) {
-    var encryptedData = response;
-    const EncryptedDataBase64Str = zlib.gzipSync(JSON.stringify(encryptedData)).toString('base64');
-    Payload['Text'] = EncryptedDataBase64Str
-    return Payload;
-  })
-  .then(function(response) {
-    var Payload = response;
-
-    //Prepare Database & Table
-    createDB()
-    .then(function(res){
-        createTable()
-        .then(function(res){
-          //Insert Record
-          storeSecret(Payload)
-          .then(function(response){
-            console.log(response);
-            return response;
+  hydrateDBCreds(DBSecret)
+  .then(function(response){
+    encryptData(KeyId, Payload['Text'])
+    .then(function(response) {
+      var encryptedData = response;
+      const EncryptedDataBase64Str = zlib.gzipSync(JSON.stringify(encryptedData)).toString('base64');
+      Payload['Text'] = EncryptedDataBase64Str
+      return Payload;
+    })
+    .then(function(response) {
+      var Payload = response;
+  
+      //Prepare Database & Table
+      createDB()
+      .then(function(res){
+          createTable()
+          .then(function(res){
+            //Insert Record
+            storeSecret(Payload)
+            .then(function(response){
+              console.log(response);
+              return response;
+            })
+            .catch(function(err){
+              console.log(err);
+            });
           })
           .catch(function(err){
             console.log(err);
           });
-        })
-        .catch(function(err){
-          console.log(err);
-        });
+      })
+      .catch(function(err){
+        console.log(err);
+      });
+      return Payload;
     })
-    .catch(function(err){
-      console.log(err);
+    .then(function(response){   
+        var output = {
+          'Message':'Data encrypted and stored, keep your key save',
+          'Key' : KeyId 
+        };
+        //console.log(response)
+        res.status(200).send( output );
+    })
+    .catch(function(err) {
+        console.log(err);
+        res.status(400).send( {'Message':'Data encryption failed, check logs for more details' });
     });
-    return Payload;
+    return response
   })
-  .then(function(response){   
-      //var Payload = response; 
-      var splitStr = KeyId.split('/');
-      var KmsId = splitStr[1];
-      var output = {
-        'Message':'Data encrypted and stored, keep your key save',
-        'Key' : KmsId 
-      };
-      res.status(200).send( output );
+  .catch(function(err){
+      console.log(err);
+      res.status(400).send( {'Message':'Failed Hydrating Credentials' });
   })
-  .catch(function(err) {
-      res.status(400).send( {'Message':'Data encryption failed, check logs for more details' });
-  });
 });
 
 router.get('/decrypt', (req, res) => {
@@ -189,32 +256,37 @@ router.get('/decrypt', (req, res) => {
     'Name': req.body.Name,
     'Key': req.body.Key
   }
-  
-
-
-  getSecret(Payload)
-  .then(function(response) {
-    var secretText = response;
-    const originalObj = JSON.parse(zlib.unzipSync(Buffer.from(secretText, 'base64')));
-    var buf = Buffer.from(originalObj, 'utf8');
-    decryptData(Payload['Key'],buf)
-    .then(function(response){
-      var output = {
-        'Text':response,
-      };
-      res.status(200).send( output );
+  hydrateDBCreds(DBSecret)
+  .then(function(response){
+    getSecret(Payload)
+    .then(function(response) {
+      var secretText = response;
+      const originalObj = JSON.parse(zlib.unzipSync(Buffer.from(secretText, 'base64')));
+      var buf = Buffer.from(originalObj, 'utf8');
+      decryptData(Payload['Key'],buf)
+      .then(function(response){
+        var output = {
+          'Text':response,
+        };
+        res.status(200).send( output );
+      })
+      .catch(function(err){
+        res.status(400).send( {'Message':'Data decryption failed, make sure you have the correct key' });
+      });
+      return response;
     })
-    .catch(function(err){
-      res.status(400).send( {'Message':'Data decryption failed, make sure you have the correct key' });
+    .catch(function(err) {
+        res.status(400).send( {'Message':'Failed getting secret text, check the user name' });
     });
-    return response;
+  return response;
   })
-  .catch(function(err) {
-      res.status(400).send( {'Message':'Failed getting secret text, check the user name' });
-  });
+  .catch(function(err){
+      console.log(err);
+      res.status(400).send( {'Message':'Failed Hydrating Credentials' });
+  })
+
+
 });
-
-
 
 
 app.use("/",router);
